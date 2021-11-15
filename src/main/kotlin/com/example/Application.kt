1a5082f1aacbb72301b7b83d3c6d22a0e152e28b
orosepa <l.olga.podgornaya@gmail.com>
2021-11-15 20:55:41 +0300

    Clean git cache

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -6,7 +6,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+    embeddedServer(Netty, port = 8081, host = "0.0.0.0") {
         configureRouting()
         initializeDb()
     }.start(wait = true)
--- src/main/resources/scripts/app.js
+++ src/main/resources/scripts/app.js
@@ -15,7 +15,7 @@ $(function(){
     if (!window["WebSocket"]) {
         alert("Error: Your browser does not support web  sockets.")
     } else {
-        socket = new WebSocket("ws://localhost:8080/webs");
+        socket = new WebSocket("ws://localhost:8081/webs");
         socket.onclose = function() {
             alert("Connection has been closed.");
         }
