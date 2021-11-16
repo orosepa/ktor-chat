358aa0fd3d1eb3b6a4522a3379fa7073d3c8da5c
orosepa <l.olga.podgornaya@gmail.com>
2021-11-16 14:42:38 +0300

    Remove .gitignore

---
+++ src/main/kotlin/com/example/DBInfo.kt
@@ -0,0 +1,11 @@
+package com.example
+
+object DBInfo {
+    val HOST: String = "ec2-54-195-246-55.eu-west-1.compute.amazonaws.com"
+    val DATABASE: String = "d4oqj5luk9c1fp"
+    val USER: String = "qgmhezcfjgftgb"
+    val PORT: Int = 5432
+    val PASSWORD: String = "895d1ef00ce696a0432f97ed3c8c9fec97d20c74f7803b76edbd380f63460f6a"
+    val URI: String =
+        "postgres://qgmhezcfjgftgb:895d1ef00ce696a0432f97ed3c8c9fec97d20c74f7803b76edbd380f63460f6a@ec2-54-195-246-55.eu-west-1.compute.amazonaws.com:5432/d4oqj5luk9c1fp"
+}
