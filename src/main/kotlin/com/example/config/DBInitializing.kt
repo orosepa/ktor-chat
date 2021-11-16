9ead19de9edfcbf437b7edd8d32ba302b130d911
orosepa <l.olga.podgornaya@gmail.com>
2021-11-16 15:23:23 +0300

    remove unused imports

--- src/main/kotlin/com/example/config/DBInitializing.kt
+++ src/main/kotlin/com/example/config/DBInitializing.kt
@@ -1,7 +1,6 @@
 package com.example.config
 
 import com.example.models.Chats
-//import com.example.models.Connections
 import com.example.models.Messages
 import io.ktor.application.*
 import org.jetbrains.exposed.sql.Database
@@ -11,13 +10,6 @@ import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.initializeDb() {
-
-//    Database.connect(url = "jdbc:postgresql://${DBInfo.HOST}:${DBInfo.PORT}/${DBInfo.DATABASE}",
-//        driver = "org.postgresql.Driver",
-//        user = DBInfo.USER,
-//        password = DBInfo.PASSWORD)
-
-//    Database.connect(url = System.getenv("DATABASE_URL"))
 
     val url = "jdbc:postgresql://${System.getenv("DATABASE_HOST")}:${System.getenv("DATABASE_PORT")}/${System.getenv("DATABASE_NAME")}"
     println(url)
--- src/main/kotlin/com/example/models/ChatDAO.kt
+++ src/main/kotlin/com/example/models/ChatDAO.kt
@@ -3,10 +3,8 @@ package com.example.models
 import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.Table
 import java.util.*
 
 object Chats : UUIDTable() {
